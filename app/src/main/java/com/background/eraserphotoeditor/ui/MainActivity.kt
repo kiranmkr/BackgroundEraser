75850b4dec37154859cbfbbbaef5f77c4606a565
kiranmkr <101919950+kiranmkr@users.noreply.github.com>
2022-06-29 05:55:18 +0500

    under_dev

--- app/src/main/java/com/background/eraserphotoeditor/ui/MainActivity.kt
+++ app/src/main/java/com/background/eraserphotoeditor/ui/MainActivity.kt
@@ -12,10 +12,12 @@ import android.util.Log
 import android.view.ViewTreeObserver
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
+import androidx.core.view.GravityCompat
 import com.example.backgrounderaser.R
 import com.example.backgrounderaser.databinding.ActivityMainBinding
 import com.background.eraserphotoeditor.utils.Constant
 import com.background.eraserphotoeditor.utils.Constants
+import com.background.eraserphotoeditor.utils.FeedbackUtils
 import com.background.eraserphotoeditor.utils.StoreManager
 import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
@@ -65,8 +67,93 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
             StoreManager.setCurrentCroppedMaskBitmap(this, null)
             openCamera()
         }
+
+        mainBinding.menuBtn.setOnClickListener {
+            mainBinding.drawer.openDrawer(GravityCompat.START)
+        }
+
+        mainBinding.navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_rate_us -> {
+                    try {
+                        startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW, Uri
+                                    .parse("market://details?id=" + "com.background.eraser.photo.editor")
+                            )
+                        )
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+
+                    }
+                }
+                R.id.nav_contact_us -> {
+                    FeedbackUtils.startFeedbackEmail(this@MainActivity)
+                }
+
+                R.id.nav_share_app -> {
+
+                    try {
+                        val i = Intent(Intent.ACTION_SEND)
+                        i.type = "text/plain"
+                        i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name))
+                        var sAux = "\nLet me recommend you this application\n\n"
+                        sAux = """
+                ${sAux}https://play.google.com/store/apps/details?id=com.background.eraser.photo.editor
+                """.trimIndent()
+                        i.putExtra(Intent.EXTRA_TEXT, sAux)
+                        startActivity(
+                            Intent.createChooser(
+                                i,
+                                resources.getString(R.string.choose_one)
+                            )
+                        )
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                    }
+
+                }
+
+                R.id.nav_privacy -> {
+                    try {
+                        startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://coreelgo.blogspot.com/privacy-policy")
+                            )
+                        )
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+
+                R.id.nav_more_app -> {
+
+                    try {
+                        startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW, Uri
+                                    .parse("market://details?id=" + "com.background.eraser.photo.editor")
+                            )
+                        )
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+
+                    }
+
+                }
+            }
+            true
+        }
     }
 
+    override fun onBackPressed() {
+        if (mainBinding.drawer.isDrawerOpen(GravityCompat.START)) {
+            mainBinding.drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
     private fun openCamera() {
 
